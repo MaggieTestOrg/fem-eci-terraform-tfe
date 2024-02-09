067da3b4770e1404689f4355ca48f4f048a43545
Maggie <maggie@nightshiftdevelopment.com>
2024-02-09 15:42:19 -0500

    Added workspaces (#1)
    Add network and cluster workspace

--- main.tf
+++ main.tf
@@ -19,10 +19,11 @@ module "workspace" {
   organization_name = var.organization_name
   project_id        = each.value.project_id
   execution_mode    = each.value.execution_mode
+  variables         = try(each.value.variables, [])
 
   # Making a 1:1 relationship between the workspace and the VCS repo
   vcs_repo = {
     github_app_installation_id = data.tfe_github_app_installation.this.id
-    identifier                 = each.value.vcs_repo_identifer
+    identifier                 = each.value.vcs_repo_identifier
   }
 }
